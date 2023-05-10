429beb64c1b265140c4b784b30cc5b5b4166f061
Yaro WebDev <yg@think.studio>
2023-05-10 10:14:08 +0300

    Added layoutsAttribute method to LayoutValue

--- src/Nova/Flexible/Resolvers/LayoutValue.php
+++ src/Nova/Flexible/Resolvers/LayoutValue.php
@@ -16,9 +16,26 @@ class LayoutValue implements \Stringable
         return Arr::get($this->data, $name);
     }
 
-    public function attribute(string $key)
+    public function attribute(string $key): mixed
     {
         return Arr::get($this->data, "attributes.{$key}");
+    }
+
+    public function layoutsAttribute(string $key): array
+    {
+        $value = $this->attribute($key);
+        if($value && is_array($value)) {
+            return array_filter(array_map(function ($item) {
+                $layoutValue = new LayoutValue(!is_array($item) ? (array)$item : $item);
+                if($layoutValue->layout && $layoutValue->key && $layoutValue->attributes) {
+                    return $layoutValue;
+                }
+
+                return null;
+            }, $value));
+        }
+
+        return [];
     }
 
     public function __toString(): string
