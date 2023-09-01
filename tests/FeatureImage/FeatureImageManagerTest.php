ccad80e19878b47331ee7c5fbc5a811b5d3d656e
Yaro WebDev <yg@think.studio>
2023-09-01 08:20:22 +0300

    Added test

--- tests/FeatureImage/FeatureImageManagerTest.php
+++ tests/FeatureImage/FeatureImageManagerTest.php
@@ -2,6 +2,7 @@
 
 namespace NovaThinKit\Tests\FeatureImage;
 
+use Illuminate\Http\UploadedFile;
 use NovaThinKit\FeatureImage\FeatureImageManager;
 use NovaThinKit\Tests\Fixtures\Models\Page;
 use NovaThinKit\Tests\TestCase;
@@ -43,6 +44,32 @@ class FeatureImageManagerTest extends TestCase
         $manager->setTag('bla_bla');
         $this->assertEquals('bla_bla', $manager->tag());
         $this->assertEquals('/storage/baz/bar/foo.png', $manager->url());
+
+    }
+
+    /** @test */
+    public function exists_function()
+    {
+        /** @var Page $page */
+        $page = Page::factory()->create();
+
+        /** @var FeatureImageManager $manager */
+        $manager = $page->featureImageManager('fooBar');
+
+        $this->assertFalse($manager->exists());
+
+        $page->{$page->featureImageKey('fooBar')} = 'foo/bar.png';
+
+        $this->assertFalse($manager->exists());
+        $this->assertFalse($manager->exists('thumb'));
+
+        $page->{$page->featureImageKey('fooBar')} = $manager->storeUploaded(UploadedFile::fake()->image('image.jpg'));
+        $page->save();
+
+        $this->assertTrue($manager->exists());
+        $this->assertTrue($manager->exists('thumb'));
+        // Invalid format anyway returns value
+        $this->assertTrue($manager->exists('foo-bar-baz'));
 
     }
 }
